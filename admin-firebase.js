608ed9ddda7629f3f351a1e02b3e96879065f1da
pypynyaa <nikita662006@bk.ru>
2025-03-13 21:25:45 +0300

    Обновлена конфигурация Firebase в админке и исправлен экспорт данных

---
+++ admin-firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA-14teCRf0g25whWzxuVGvboO_8a8hCmM",
+    authDomain: "party31-19af4.firebaseapp.com",
+    projectId: "party31-19af4",
+    storageBucket: "party31-19af4.firebasestorage.app",
+    messagingSenderId: "692315230973",
+    appId: "1:692315230973:web:10871adfa544f501706494",
+    measurementId: "G-06K7VD5GD6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db }; 
